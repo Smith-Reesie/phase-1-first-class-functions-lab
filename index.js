666382cdc2bafb0e1fbab7bcc1ce6a302e99dd64
Maurice Smith <Xmaurice.smith@gmail.com>
2021-10-24 17:30:39 -0400

    commit tree aec1eb1d1f948cd3baac7884bb831888a3ad6bf2

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+
+function returnFirstTwoDrivers(drivers) {
+    return [drivers[0], drivers[1]]
+}
+
+function returnLastTwoDrivers(drivers) {
+return drivers.slice(-2)
+    
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const fareTripler = createFareMultiplier(3)
+const fareDoubler = createFareMultiplier(2)
+
+
+function createFareMultiplier(initial) {
+    return function(secondNumber) {
+        return initial * secondNumber
+    }
+}
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers)
+}
